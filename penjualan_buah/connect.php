0e1d72a75e8ee5415927661810886ffeb9cbc069
Natalia <114990082+shanatta@users.noreply.github.com>
2023-05-31 10:17:42 +0700

    Create connect.php

---
+++ penjualan_buah/connect.php
@@ -0,0 +1,7 @@
+<?php 
+
+$db = mysqli_connect("localhost", "root", "", "db_penjualanbuah");
+
+$data = mysqli_query($db, "SELECT * FROM tbl_buah, tbl_hargabuah, tbl_pembeli WHERE tbl_buah.id_buah=tbl_hargabuah.id_buah AND tbl_pembeli.id_pembeli=tbl_hargabuah.id_pembeli ORDER BY tanggal ASC");
+
+?>
