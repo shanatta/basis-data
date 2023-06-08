f54643aaebfe10d54aad1ebdfcc86e9f7f7cf847
Natalia <114990082+shanatta@users.noreply.github.com>
2023-06-08 09:00:41 +0700

    Delete penjualan_buah directory

--- penjualan_buah/connect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-// Koneksi ke database
-$db = mysqli_connect("localhost", "root", "", "db_penjualanbuah");
-
-// Koneksi ke table
-$data = mysqli_query($db, "SELECT * FROM tbl_buah, tbl_hargabuah, tbl_pembeli WHERE tbl_buah.id_buah=tbl_hargabuah.id_buah AND tbl_pembeli.id_pembeli=tbl_hargabuah.id_pembeli ORDER BY tanggal ASC");
-
-?>
